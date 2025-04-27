c53b7fffc7beacb16127c881820ee03dd74b2045
parthrkushwaha <parthkushwahapk2003@gmail.com>
2025-04-27 23:28:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let submit = document.getElementById("submit");
+let namefield=document.getElementById("namefield");
+let title=document.getElementById("title");
+
+submit.onclick=function(){
+    namefield
+}
